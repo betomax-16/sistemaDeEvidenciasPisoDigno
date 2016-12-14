af94602ecdbd86bae33b888a3bea398d44e21345
Angel Emilio Lopez Fernandez <Appjak@iMac-de-Angel.local>
2016-12-14 17:48:17 -0600

    Nueva Interfaz

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,36 +2,33 @@
 <html lang="en">
 
 <head>
-    <title>GSUPPuebla</title>
     <!-- Required meta tags always come first -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-
-    <!-- Cargando Fuentes-->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,700i" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
     <!-- Cargando Iconos-->
-    <link href='css/font-awesome.min.css' rel="stylesheet" type="text/css">
+    <link href='dist/css/font-awesome.min.css' rel="stylesheet" type="text/css">
+    <!-- Carga de Archivos CSS -->
+    <link rel="stylesheet" href="dist/css/estilos.css">
 
-
-
-    <!-- Carga de rchivos Css-->
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/Estilos.css">
 </head>
 
+
 <body>
-    <section class="Bienvenidos ">
+    <section class="Top">
 
         <header class="encavezado navbar-fixed-top" role="banner">
             <div class="container">
-                <a href="/resources/welcome.blade.php" class="logo">
+                <a href="#" class="logo">
                     <img src="images/logo.svg" alt="logo_del_sitio ">
                 </a>
+                <button class="c-hamburger c-hamburger--htx boton-menu hidden-md-up" data-toggle="collapse" data-target="#menu-principal" aria-expanded="false">
+                    <span>toggle menu</span>
+                </button>
 
-                <button type="button" class="boton-menu hidden-md-up" data-toggle="collapse" data-target="#menu-principal" aria-expanded="false">
-                    <i class="fa fa-bars" aria-hidden="true"></i></button>
 
                 <nav class="collapse" id="menu-principal">
                     <ul>
@@ -44,187 +41,36 @@
                 </nav>
             </div>
         </header>
+    </section>
 
+
+    <section class="Bienvenidos text-xs-center">
         <div class="container">
             <div class="row">
-                <div class="logo2 col-xs-12 col-md-5">
-                    <img src="images/logo2.svg" alt="logo_del_sitio ">
+                <h1 class="display-3 col-xs-12 col-md-4">Ayudanos <br> a <br> <span>Ayudar</span></h1>
+                <div class="slide offset-md-2 col-md-4 hidden-md-down">
+                    <p>holi</p>
+
                 </div>
+
             </div>
-            <div class="texto-encavezado text-xs-center">
-                <a href="Contacto.html" class="btn btn-secondary btn-lg ">Contactanos</a>
+            <div class="texto-encavezado ">
+                <a href="#" class="btn btn-secondary btn-lg ">Contactanos</a>
             </div>
 
-            <div class="flecha_bajar text-xs-center">
+            <div class="flecha_bajar">
                 <a href="#nosotros"><i class="fa fa-angle-down" aria-hidden="true"></i></a>
             </div>
-
-    </section>
-    <!-- Carga de archivos Css -->
-
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-
-    <section class="nosotros " id="nosotros">
-        <h1 class="h2 text-xs-center">Nosotros</h1>
-        <div class="linea"></div>
-        <br>
-        <div class="container text-xs-center">
-
-            <div class="row">
-                <div class="col-xs-12  col-md-5">
-                    <p>Red Colaborativa es una plataforma dirigida al control y seguimiento detallado de proyectos destinado a eventos, congresos y concursos en los cuales es complicado llevar un control de los mismos, su objetivo fundamental es la busqueda de colaboradores que ayuden a impulsar el desarrollo de calidad e innovación de los proyectos tecnológicos.</p>
-
-
-                </div>
-                <div class="col-xs-12 col-md-6 offset-md-1">
-                    <img src="images/Nosotros1.jpg" alt="Nosotros2">
-                    <br>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12  col-md-6">
-                    <img src="images/Nosotros2.jpg" alt="Nosotros2">
-                    <br>
-                </div>
-                <div class="col-xs-12  col-md-5 offset-md-1">
-                    <p>Red Colaborativa es una plataforma dirigida al control y seguimiento detallado de proyectos destinado a eventos, congresos y concursos en los cuales es complicado llevar un control de los mismos, su objetivo fundamental es la busqueda de colaboradores que ayuden a impulsar el desarrollo de calidad e innovación de los proyectos tecnológicos.</p>
-                    <br>
-                </div>
-            </div>
-        </div>
-
-    </section>
-    <section class="lo-que-hacemos p-a-1">
-
-        <div class="container">
-            <h2 class="h3 text-xs-center ">¿Que es lo <span>que hacemos?</span></h2>
-            <p class="text-xs-center">
-                Ayudandanos a ayudar
-            </p>
-            <div class="row">
-                <div class="linea"></div>
-                <br>
-                <ul class="col-xs-6 col-lg-4 text-xs-center text-lg-left">
-                    <li>
-
-                        <i class="fa fa-home" aria-hidden="true"></i>
-                        <div class="contenedor-eleccion">
-                            <h4>Ponemos piso firme </h4>
-                            <p class="hidden-md-down">Se pone piso firme a las casas con piso de tierra o en peor estado.</p>
-                        </div>
-                    </li>
-                    <li>
-                        <i class="fa fa-inbox" aria-hidden="true"></i>
-                        <div class="contenedor-eleccion">
-                            <h4>Entregamos despensas </h4>
-                            <p class="hidden-md-down">A las Familias de escasos recursos se les entrega una despensa cada mes.</p>
-                        </div>
-                    </li>
-                    <li>
-                        <i class="fa fa-wheelchair" aria-hidden="true"></i>
-                        <div class="contenedor-eleccion">
-                            <h4>Ayudamos a los menos aforunados</h4>
-                            <p class="hidden-md-down">Se entregan, sillas de ruedas, bastones y demas a quienes mas lo necesitan.</p>
-                        </div>
-                    </li>
-                </ul>
-
-                <div class="hidden-md-down col-lg-4">
-                    <img src="images/mision.png" alt="mision">
-                </div>
-
-                <ul class="col-xs-6 col-lg-4 text-xs-center text-lg-right">
-                    <li>
-                        <i class="fa fa-home" aria-hidden="true"></i>
-                        <div class="contenedor-eleccion">
-                            <h4>Ponemos piso firme </h4>
-                            <p class="hidden-md-down">Se pone piso firme a las casas con piso de tierra o en peor estado.</p>
-                        </div>
-                    </li>
-                    <li>
-                        <i class="fa fa-inbox" aria-hidden="true"></i>
-                        <div class="contenedor-eleccion">
-                            <h4>Entregamos despensas </h4>
-                            <p class="hidden-md-down">A las Familias de escasos recursos se les entrega una despensa cada mes.</p>
-                        </div>
-
-                    </li>
-                    <li>
-                        <i class="fa fa-wheelchair" aria-hidden="true"></i>
-                        <div class="contenedor-eleccion">
-                            <h4>Ayudamos a los menos aforunados</h4>
-                            <p class="hidden-md-down">Se entregan, sillas de ruedas, bastones y demas a quienes mas lo necesitan.</p>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </section>
-
-    <section class="mision-vision text-center">
-        <div class="mision">
-            <h1>Mision</h1>
-            <div class="linea"></div>
-            <br>
-            <p>La misión de Red Colaborativa es ayudar y ser una buena opción para la organización de eventos al crear, publicar, gestionar y difundir todo tipo de proyectos tecnologicos para fomentar e impulsar un entorno colaborativo de investigación tecnológica, científica, etc. A la vez ser una herramienta flexible para adaptarse a eventos de acuerdo a las necesidades, estableciendo así relaciones de comunicación e interacción con otros usuarios.</p>
-            <img src="images/mision2.svg" alt="mision2">
-
-
-        </div>
-
-        <div class="vision">
-            <h1>Vision</h1>
-            <div class="linea"></div>
-            <br>
-            <p>Ser la mejor plataforma tecnologica de trabajo colaborativo enfocado a proyectos, impulsando la creatividad, innovacion e invencion de productos o servicios de alta calidad, permitiendo tener un mejor rendimiento productivo, operativo y colaborativo.</p>
-
-
         </div>
     </section>
 
 
-    <main class="programas p-y1">
-        <div class="container">
-            <h3 class="text-xs-center font-weight-bold">Nuestros Programas</h3>
-            <div class="row">
-
-                <article class="col-md-4">
-                    <img src="images/LogoPiso.svg" alt="LogoPiso Digno">
-                    <h3><a href="#">Piso Digno</a></h3>
-                    <p class="hidden-sm-down">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia natus asperiores quo eaque, reiciendis provident nobis voluptatem. Quos repellat excepturi cupiditate earum exercitationem porro tempore, odit consectetur fugit ipsa facere.</p>
-                    <a href="#" class="btn btn-secondary hidden-sm-down">Más Informacion</a>
-                </article>
-
-                <article class="col-md-4">
-                    <img src="images/LogoPiso.svg" alt="LogoPiso Digno">
-
-                    <h3><a href="#">Despensas</a></h3>
-                    <p class="hidden-sm-down">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia natus asperiores quo eaque, reiciendis provident nobis voluptatem. Quos repellat excepturi cupiditate earum exercitationem porro tempore, odit consectetur fugit ipsa facere.</p>
-                    <a href="#" class="btn btn-secondary hidden-sm-down">Más Informacion</a>
-                </article>
-                <article class="col-md-4">
-                    <img src="images/LogoPiso.svg" alt="LogoPiso Digno">
-                    <h3><a href="#">Cooperaticva</a></h3>
-                    <p class="hidden-sm-down">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia natus asperiores quo eaque, reiciendis provident nobis voluptatem. Quos repellat excepturi cupiditate earum exercitationem porro tempore, odit consectetur fugit ipsa facere.</p>
-                    <a href="#" class="btn btn-secondary hidden-sm-down">Más Informacion</a>
-                </article>
-            </div>
-        </div>
-    </main>
-
-
-    <footer class="piedepagina p-y-1 " role="contentinfo">
-        <div class="container">
-            <p>2016 c GSUPPuebla Todos los derechos reservados</p>
-            <ul class="redes-sociales">
-                <li><a href="#"><i class="fa fa-facebook " aria-hidden="true"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter " aria-hidden="true"></i></a></li>
-                <li><a href="#"><i class="fa fa-youtube " aria-hidden="true"></i></a></li>
-            </ul>
-        </div>
-    </footer>
-    <a class="ir-arriba" href="#"><i class="fa fa-arrow-circle-up " aria-hidden="true"></i></a>
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="dist/js/estilos.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
 </body>
+
 
 </html>
