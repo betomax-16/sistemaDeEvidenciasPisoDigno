c10cb7d37ae4aaf1fdfce4a255026ce90c3ffbc4
Roberto Castillo <betomax_16@live.com.mx>
2017-01-29 18:03:32 -0600

    asignando links a iconos de redes sociales

--- resources/views/layouts/menu/footer.blade.php
+++ resources/views/layouts/menu/footer.blade.php
@@ -9,8 +9,8 @@
                 <p>© 2017 Grupos Sociales Unidos por Puebla 13 de Noviembre A.C. | Derechos reservados | <a href="{{route('avisop')}}">Aviso de Privacidad</a></p>
               </div>
               <div class="col-md-4 redes-sociales text-xs-center text-md-right">
-                <a href="#"><i class="fa fa-facebook " aria-hidden="true"></i></a>
-                <a href="#"><i class="fa fa-twitter " aria-hidden="true"></i></a>
+                <a href="https://www.facebook.com/gsuppuebla/" target="_blank"><i class="fa fa-facebook " aria-hidden="true"></i></a>
+                <a href="https://twitter.com/gsuppuebla" target="_blank"><i class="fa fa-twitter " aria-hidden="true"></i></a>
                 <a href="#"><i class="fa fa-youtube " aria-hidden="true"></i></a>
               </div>
             </div>
