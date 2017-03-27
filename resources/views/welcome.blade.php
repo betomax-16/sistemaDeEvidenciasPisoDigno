ae381f673edda114322b71d63bea779eea1ad872
Angel Emilio Lopez Fernandez <Appjak@iMac-de-Angel.local>
2017-03-27 17:56:42 -0600

    Carrousel sin bolitas y con flechas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -140,21 +140,7 @@
 
 
                 <div id="carouselExampleIndicators" class="carousel slide col-xs-12 offset-md-1 col-md-5" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="10"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="11"></li>
-
-
-                    </ol>
+ 
                     <div class="carousel-inner" role="listbox">
                         <div class="carousel-item active">
                             <img class="d-block img-fluid" src="{{asset('imagenes/fotos/file1.jpg')}}" alt="First slide">
@@ -191,14 +177,12 @@
                         </div>
 
                     </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
+                   <a class="left carousel-control" href="#carouselExampleIndicators" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#carouselExampleIndicators" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
                 </div>
             </div>
         </div>
